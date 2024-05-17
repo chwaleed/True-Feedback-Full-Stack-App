54d28cf450962abe62a7462e8427c2607ee1a04f
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-05-17 23:30:37 +0500

    User Scehma added

---
+++ src/models/User.js
@@ -0,0 +1,13 @@
+import mongoose, { Schema } from "mongoose";
+
+const MessageSchema = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
