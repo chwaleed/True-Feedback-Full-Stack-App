920f91343171521c63212a71f4ab87a1a74c1aad
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-05-17 23:42:04 +0500

    User Schema is added

--- src/models/User.js
+++ src/models/User.js
@@ -11,3 +11,42 @@ const MessageSchema = new Schema({
     default: Date.now,
   },
 });
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    match: [
+      /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/,
+      "Please use a valid Email",
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "Verify Code is required"],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "Verify Code Expiry is required"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    default: false,
+  },
+  message: [MessageSchema],
+});
