753e8b4a69133f290b4a19fa7eb8dd845bbf71ef
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-05-18 23:03:19 +0500

    resend work done

--- src/helpers/sendVerificationEmail.js
+++ src/helpers/sendVerificationEmail.js
@@ -1,1 +1,17 @@
 import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/verificationEmail";
+
+export async function sendVerificationEmail(email, username, verifyCode) {
+  try {
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: email,
+      subject: "Mystry message | Verification Code",
+      react: VerificationEmail({ username, otp: verifyCode }),
+    });
+    return { success: true, message: "Vefication email send successfuly" };
+  } catch (emailError) {
+    console.error("Error sending vefication email", emailError);
+    return { success: false, message: "Failed to send verification email" };
+  }
+}
