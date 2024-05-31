ef28ff0c389221703c075be90e0ff62e4f737db6
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-05-31 15:14:01 +0500

    Checking user name unique route is completed

--- src/app/api/check-username-unique/route.js
+++ src/app/api/check-username-unique/route.js
@@ -16,6 +16,33 @@ export async function GET(request) {
       username: searchParams.get("username"),
     };
     const result = UsernameQuerySchema.safeParse(queryParam);
+    console.log(result); // remove
+    if (!result.success) {
+      const usernameError = result.error.format().username?._errors || [];
+      return NextResponse.json({
+        success: false,
+        message: "invalid Qory Parameters",
+      });
+    }
+
+    const { username } = result.data;
+    const existingVerifiedUser = UserModel.findOne({
+      username,
+      isVarified: true,
+    });
+    if (existingVerifiedUser) {
+      return NextResponse.json({
+        success: false,
+        message: "user name already taken",
+      });
+    }
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Username is Unique",
+      },
+      { status: 400 }
+    );
   } catch (error) {
     console.error("Error in checking username", error);
     return NextResponse.json(
