9cedf848deb6187aa73b161f9ba7410e399b0f49
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-06-01 00:05:48 +0500

    verify-code Route is added

---
+++ src/app/api/verify-code/route.js
@@ -0,0 +1,60 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/User";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  await dbConnect();
+  try {
+    const reqBody = await request.json();
+    const { verifyCode, username } = reqBody;
+
+    const decodedUsername = decodeURIComponent(username); // NOt needed but for info
+    const user = await UserModel.findOne({ username: decodedUsername });
+    if (!user) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "User not Found",
+        },
+        { status: 500 }
+      );
+    }
+    const isCodeValid = user.verifyCode === verifyCode;
+    const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+    if (isCodeValid && isCodeNotExpired) {
+      user.isVerified = true;
+      await user.save();
+      return NextResponse.json(
+        {
+          success: true,
+          message: "User Verified",
+        },
+        { status: 200 }
+      );
+    } else if (!isCodeNotExpired) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Code is Expired",
+        },
+        { status: 400 }
+      );
+    } else {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Incorrect Code",
+        },
+        { status: 400 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Error in Verifing User",
+      },
+      { status: 500 }
+    );
+  }
+}
