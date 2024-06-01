a07e80a8ef112df041351b27c96d63913258f1ff
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-06-01 21:39:07 +0500

    accept-messages route added

---
+++ src/app/api/accept-messages/route.js
@@ -0,0 +1,4 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import { User } from "next-auth";
