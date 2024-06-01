b88ee85927b8bf13ea67ef352f290f0a751b3066
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-06-01 21:43:28 +0500

    user and user id is extracted from the session

--- src/app/api/accept-messages/route.js
+++ src/app/api/accept-messages/route.js
@@ -1,4 +1,22 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/options";
 import dbConnect from "@/lib/dbConnect";
-import { User } from "next-auth";
+import UserModel from "@/models/User";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  await dbConnect();
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+  if (!session || !session.user) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Not authenticated",
+      },
+      { status: 401 }
+    );
+  }
+  const userId = user._id;
+  const { acceptMessages } = await request.json();
+}
