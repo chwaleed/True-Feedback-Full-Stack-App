810c218b32d7e50f14c6108e43f814684d347e6e
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-06-21 11:08:15 +0500

    checking username unique

--- src/app/(auth)/sign-in/page.js
+++ src/app/(auth)/sign-in/page.js
@@ -3,9 +3,45 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import Link from "next/link";
+import { useState, useEffect } from "react";
+import { useDebounceValue } from "usehooks-ts";
+import { useRouter } from "next/navigation";
+import { signUpSchema } from "@/models/signUpSchema";
+import axios from "axios";
 
-function page() {
+function Page() {
+  const [username, setUsername] = useState("");
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [isLoading, setIsloading] = useState(false);
+  const [isSubmiting, setIsSubmiting] = useState(false);
+  const router = useRouter();
+  const deboundedUsername = useDebounceValue(username, 300);
+
+  // Implementing Zod
+  const form = useForm({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  useEffect(() => {
+    const checkUsernameUnique = async () => {
+      if (deboundedUsername) {
+        setIsloading(true);
+        setUsernameMessage("");
+        try {
+          await axios.get(
+            `/api/check-username-unique?username=${deboundedUsername}`
+          );
+        } catch (error) {}
+      }
+    };
+  }, [deboundedUsername]);
+
   return <div>page</div>;
 }
 
-export default page;
+export default Page;
