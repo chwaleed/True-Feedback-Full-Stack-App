d2913f54794d3db73eff5a77a5ab76404e7d6b76
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-06-23 23:18:34 +0500

    form added

--- src/app/(app)/dashboard/page.js
+++ src/app/(app)/dashboard/page.js
@@ -1,5 +1,9 @@
 "use client";
+import { acceptMessageSchema } from "@/models/acceptMessageSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useSession } from "next-auth/react";
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function Dashboard() {
   const [messages, setMessages] = useState([]);
@@ -9,6 +13,12 @@ function Dashboard() {
   const handleDeleteMessage = (messageId) => {
     setMessages(messages.filter((message) => message._id !== messageId));
   };
+  const { data: session } = useSession();
+  const form = useForm({
+    resolver: zodResolver(acceptMessageSchema),
+  });
+  const { register, watch, setValue } = form;
+  const acceptMessages = watch("acceptMessages");
   return <div>Dashboard</div>;
 }
 
