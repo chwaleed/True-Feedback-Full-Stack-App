df5501be9c3ad9c3b4e92dbb15a5513524931915
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-06-24 19:50:52 +0500

    problem in fetching messages

--- src/app/api/get-messages/route.js
+++ src/app/api/get-messages/route.js
@@ -9,7 +9,7 @@ export async function GET(request) {
   await dbConnect();
   const session = await getServerSession(authOptions);
   const user = session?.user;
-  console.log(user);
+  // console.log(user);
   if (!session || !session.user) {
     return NextResponse.json(
       {
@@ -21,13 +21,17 @@ export async function GET(request) {
   }
   const userId = mongoose.Types.ObjectId.createFromHexString(user._id);
   // const userId = new mongoose.Types.ObjectId(user._id);s
+  console.log(userId);
   try {
+    const user1 = await UserModel.aggregate([{ $match: { id: userId } }]);
+    console.log(user1);
     const user = await UserModel.aggregate([
       { $match: { id: userId } },
       { $unwind: "$messages" },
       { $sort: { "messages.createdAt": -1 } },
       { $group: { _id: "$_id", messages: { $push: "$messages" } } },
     ]);
+    console.log(user);
     if (!user || user.length === 0) {
       return NextResponse.json(
         {
@@ -44,5 +48,13 @@ export async function GET(request) {
       },
       { status: 200 }
     );
-  } catch (error) {}
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Error in gettting Messages",
+      },
+      { status: 401 }
+    );
+  }
 }
