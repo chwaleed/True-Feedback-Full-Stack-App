1eb547f5d0ccae0201f999b1deb0cb9f2d33d43e
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-06-25 13:05:01 +0500

    updated send message

--- src/app/(app)/u/[user]/page.js
+++ src/app/(app)/u/[user]/page.js
@@ -2,60 +2,72 @@
 import { useParams } from "next/navigation";
 import React from "react";
 import { useForm } from "react-hook-form";
+import { Form, FormControl } from "@/components/ui/form";
 import {
   FormField,
-  FormControl,
   FormItem,
   FormLabel,
-  FormDescription,
   FormMessage,
-  Form,
 } from "@/components/ui/form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { messageSchema } from "@/models/messageSchema";
 import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Textarea } from "@/components/ui/textarea";
+import axios from "axios";
+import { messageSchema } from "@/models/messageSchema";
+
+export const dynamic = "force-dynamic";
+
 function SendMessage() {
-  const { user } = useParams();
+  const { user: username } = useParams();
   const form = useForm({
     resolver: zodResolver(messageSchema),
+    defaultValues: {
+      content: "",
+    },
   });
-  const onSubmit = async () => {
-    console.log("Hello");
+  const onSubmit = async (data) => {
+    try {
+      const response = await axios.post("/api/send-message", {
+        username,
+        content: data.content,
+      });
+      console.log(response.data.message);
+    } catch (error) {
+      console.log("Error in sending message");
+    }
   };
+
   return (
     <div>
       <h1 className="text-center text-5xl font-bold mt-16">
         Public Profile Link
       </h1>
+
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-6 w-[50%]    mt-8 mx-auto"
+          className="space-y-6 w-[50%] mt-8 mx-auto"
         >
           <FormField
-            name="message"
+            name="content"
             control={form.control}
             render={({ field }) => (
               <FormItem>
                 <FormLabel className="font-semibold text-[1.2rem]">
-                  Send Anonymous Message to @{user}
+                  Send Anonymous Message to @{username}
                 </FormLabel>
-                <Textarea
-                  placeholder="Write your anonymous message here"
-                  {...field}
-                  className=" placeholder:text-[1.1rem] text-[1.1rem] font-semibold"
-                />
+                <FormControl>
+                  <Textarea
+                    placeholder="Write your anonymous message here"
+                    {...field}
+                    className=" placeholder:text-[1.1rem] text-[1.1rem] font-semibold"
+                  />
+                </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
-          <Button
-            className=" scale-125 left-[50%] absolute translate-x-[-50%] "
-            type="submit"
-          >
-            Send It
-          </Button>
+          <Button type="submit">Send It</Button>
         </form>
       </Form>
     </div>
